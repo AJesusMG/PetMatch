04c72be83bf1fa66eeddd5558a899a465cfe6192
BryanBonilla <bbonila04@alumnos.uaq.mx>
2023-11-07 14:55:01 -0600

    PR with dev

--- src/pages/Catalogue/Catalogue.jsx
+++ src/pages/Catalogue/Catalogue.jsx
@@ -3,13 +3,19 @@ import PostCard from "../../../components/PostCard/PostCard";
 import FullPost from "../../../components/FullPost/fullPost";
 import AdoptFullPost from "../../../components/AdoptFullPost/adoptFullPost";
 import ReportPost from "../../../components/ReportPost/reportPost";
+import SideNav from "../../../components/SideNav/sideNav.jsx";
+
 
 export default function Catalogue() {
   return (
+    
     <div>
       {/* <PostCard /> */}
       {/* <FullPost /> */}
       {/* <AdoptFullPost /> */}
+      <div>
+        <SideNav />
+      </div>
       <ReportPost />
     </div>
   );
