56fea230a41b9533aef062f08e923cfeaf714fd8
Alan J Morales <amorales130@alumnos.uaq.mx>
2023-11-07 15:06:28 -0600

    Added SideBar

--- src/pages/Catalogue/Catalogue.jsx
+++ src/pages/Catalogue/Catalogue.jsx
@@ -3,10 +3,14 @@ import PostCard from "../../../components/PostCard/PostCard";
 import FullPost from "../../../components/FullPost/fullPost";
 import AdoptFullPost from "../../../components/AdoptFullPost/adoptFullPost";
 import ReportPost from "../../../components/ReportPost/reportPost";
+import SideBar from "../../../components/SideBar/sideBar";
 
 export default function Catalogue() {
   return (
     <div>
+      <div>
+        <SideBar />
+      </div>
       {/* <PostCard /> */}
       {/* <FullPost /> */}
       {/* <AdoptFullPost /> */}
