0759eb84d2860c1301d996593103876fcad77ba7
Emiliano Aquetzalli Obregon Reyes <eobregon05@alumnos.uaq.mx>
2023-12-08 12:55:44 -0600

    modal para subir posts

--- components/ChoseTypePet/choseTypePet.jsx
+++ components/ChoseTypePet/choseTypePet.jsx
@@ -5,68 +5,144 @@ import Button from '@mui/material/Button';
 import styles from './choseTypePetStyles.module.css';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
+import { TextField, MenuItem } from '@mui/material';
 import UploadImage from '../UploadImage/uploadImage';
+
 
 const data = [
   {
     id: 1,
-    PetName: "Perro",
+    petType: "Perro",
     img: "../../public/img/Perro_F.jpg",
   },
   {
     id: 2,
-    PetName: "Gato",
+    petType: "Gato",
     img: "../../public/img/Cats.jpg",
   },
   {
     id: 3,
-    PetName: "Roedor",
+    petType: "Roedor",
     img: "../../public/img/Hamsters.jpg",
   },
   {
     id: 4,
-    PetName: "Ave",
+    petType: "Ave",
     img: "../../public/img/Birds.jpg",
   },
 ];
 
 export default function ChoseTypePet() {
-  const [open, setOpen] = React.useState(false);
-  const [uploadImageOpen, setUploadImageOpen] = useState(false);
+  const [openFirstModal, setOpenFirstModal] = useState(false);
+  const [openSecondModal, setOpenSecondModal] = useState(false);
+  const [selectedPetType, setSelectedPetType] = useState('');
+  const [openThirdModal, setOpenThirdModal] = useState(false);
 
-  const handleOpen = (mascota) => {
+  const [petData, setPetData] = useState({
+    name: '',
+    breed: '',
+    pet_Size: '',
+    age: '',
+    description: '',
+    owner: 1,
+    allergies: '',
+    exercise_ability: '',
+    status: 'en adopcion'
+  });
 
-    setOpen(false);
-    setUploadImageOpen(true);
+  const handleOpenFirstModal = () => {
+    setOpenFirstModal(true);
   };
-  const handleClose = () => {
-    setOpen(true);
+
+  const handleCloseFirstModal = () => {
+    setOpenFirstModal(false);
   };
-  const handleCloseUploadImage = () => {
-    setUploadImageOpen(false);
-    setOpen(true); // Puedes configurar el modal principal en true si deseas regresar a él
+
+  const handleOpenSecondModal = (mascota) => {
+    setSelectedPetType(mascota.petType);
+    setOpenFirstModal(false);
+    setOpenSecondModal(true);
   };
+
+  const handleCloseSecondModal = () => {
+    setOpenSecondModal(false);
+    setOpenFirstModal(true);
+    setSelectedPetType('');
+    setPetData({
+      name: '',
+      breed: '',
+      age: '',
+      pet_Size: '',
+      description: '', // Agregar limpieza para el campo 'description'
+      // Si este campo no debería reiniciarse, déjalo fuera
+      allergies: '',
+      exercise_ability: '', // Limpiar otros campos no relacionados con la creación de mascotas
+      // Si este campo no debería reiniciarse, déjalo fuera
+    });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setPetData({ ...petData, [e.target.name]: e.target.value });
+  };
+
+  const handleNextButtonClick = () => {
+    // Aquí puedes completar los datos de la mascota con el tipo seleccionado y los datos del formulario
+    const completePetData = {
+      ...petData,
+      type: selectedPetType,
+    };
+    console.log('Datos de la mascota:', completePetData);
+    // Realizar acciones adicionales o enviar a tu backend aquí
+
+    fetch('http://localhost:3000/pets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(completePetData)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        console.log('Pet created:', data);
+        setOpenSecondModal(false); // Cierra el segundo modal
+        setOpenThirdModal(true); // Abre el tercer modal
+      })
+      .catch(error => {
+        console.error('There was an error creating the pet:', error);
+        // Manejar el error de la solicitud
+      });
+
+  };
+
+
+
 
   return (
     <div>
-      <Button onClick={() => setOpen(true)} style={{ borderRadius: '50%', width: '40px', height: '50px', color: '#000' }}><AddIcon /></Button>
+      <Button onClick={handleOpenFirstModal} style={{ borderRadius: '50%', width: '40px', height: '50px', color: '#000' }}><AddIcon /></Button>
       <Modal
-        open={open}
-        onClose={() => setOpen(false)}
+        open={openFirstModal}
+        onClose={handleCloseFirstModal}
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
         <Box className={styles.modalContainer}>
-        <div className={styles.headModalChooseType}>
-            <Button style={{ width: '30px', height: '30px', color: '#000', padding: '0'}} className={styles.headModalChooseTypeCloseButtton} onClick={handleClose}><CloseIcon/></Button>
+          <div className={styles.headModalChooseType}>
+            <Button style={{ width: '30px', height: '30px', color: '#000', padding: '0' }} className={styles.headModalChooseTypeCloseButtton} onClick={handleCloseFirstModal}><CloseIcon /></Button>
             <h2 className={styles.headModalChooseTypeTitle}>Elige qué es...</h2>
           </div>
           <div className={styles.headModalChooseTypeImageGrid}>
             {data.map((mascota) => (
-              <div className={styles.imgContainer} key={mascota.id} onClick={() => handleOpen(mascota)}>
+              <div className={styles.imgContainer} key={mascota.id} onClick={() => handleOpenSecondModal(mascota)}>
                 <img className={styles.imgAnimal} src={mascota.img} alt="Animal" />
                 <div className={styles.overlay}>
-                  <p>{mascota.PetName}</p>
+                  <p>{mascota.petType}</p>
                 </div>
               </div>
             ))}
@@ -74,14 +150,86 @@ export default function ChoseTypePet() {
         </Box>
       </Modal>
       <Modal
-        open={uploadImageOpen}
-        onClose={handleCloseUploadImage}
+        open={openSecondModal}
+        onClose={handleCloseSecondModal}
         aria-labelledby="upload-image-modal-title"
         aria-describedby="upload-image-modal-description"
       >
+        <form onSubmit={handleFormSubmit} >
+          <Box className={styles.modalContainer}>
+            <div className={styles.headModalAddInfo}>
+              <Button style={{ width: '30px', height: '30px', color: '#000', padding: '0' }} className={styles.headModalChooseTypeCloseButtton} onClick={handleCloseSecondModal}>Atras</Button>
+              <h2 className={styles.headModalAddInfoTitle} >Ingresa los datos de tu mascota</h2>
+              <Button onClick={handleNextButtonClick}>Siguiente</Button>
+            </div>
+            <div className={styles.formInfoPet}>
+              <div style={{ display: 'flex', gap: '10px' }}>
+                <TextField
+                  name="name"
+                  label="Name"
+                  value={petData.name}
+                  onChange={handleFormSubmit}
+                  style={{ flex: '1' }}
+                />
+                <TextField
+                  name="age"
+                  label="Age"
+                  value={petData.age}
+                  onChange={handleFormSubmit}
+                  style={{ flex: '1' }}
+                />
+              </div>
+              <div style={{ display: 'flex', gap: '10px' }}>
+                <TextField
+                  name="pet_Size"
+                  label="Size"
+                  value={petData.pet_Size}
+                  onChange={handleFormSubmit}
+                  style={{ flex: '1' }}
+                />
+                <TextField
+                  name="breed"
+                  label="Breed"
+                  value={petData.breed}
+                  onChange={handleFormSubmit}
+                  style={{ flex: '1' }}
+                />
+              </div>
+              <TextField name="allergies" label="Allergies" value={petData.allergies} onChange={handleFormSubmit} /> {/* Agregado */}
+              <TextField
+                select
+                name="exercise_ability"
+                label="Exercise Ability"
+                value={petData.exercise_ability}
+                onChange={handleFormSubmit}
+              >
+                <MenuItem value="Poco">Poco</MenuItem>
+                <MenuItem value="Moderado">Moderado</MenuItem>
+                <MenuItem value="Mucho">Mucho</MenuItem>
+              </TextField>
+              <TextField
+                name="description"
+                label="Description"
+                value={petData.description}
+                onChange={handleFormSubmit}
+                multiline
+                rows={4}
+              />
+            </div>
+
+          </Box>
+        </form>
+      </Modal>
+      <Modal
+        open={openThirdModal}
+        onClose={() => setOpenThirdModal(false)}
+        aria-labelledby="third-modal-title"
+        aria-describedby="third-modal-description"
+      >
         <Box className={styles.modalContainer}>
-          <UploadImage/>
-          <Button onClick={handleCloseUploadImage}>Volver</Button>
+          {/* Contenido del tercer modal */}
+          {/* ... */}
+          <UploadImage />
         </Box>
       </Modal>
     </div>
--- components/UploadImage/uploadImage.jsx
+++ components/UploadImage/uploadImage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import styles from './uploadImageStyles.module.css';
 import CloudQueueIcon from '@mui/icons-material/CloudQueue';
@@ -6,40 +6,86 @@ import CloseIcon from '@mui/icons-material/Close';
 import { styled } from '@mui/material/styles';
 
 const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
 });
 
-export default function uploadImage() {
+export default function UploadImage() {
+  const [selectedFile, setSelectedFile] = useState(null);
 
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-    return (
-        <React.Fragment>
-            <div className={styles.modalContainer}>
+  const handleUpload = () => {
+    // Hacer la petición POST a localhost:3000/uploads con el archivo seleccionado
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('image', selectedFile);
+;
 
-                <div className={styles.headModalUploadImage}>
-                    <Button style={{ width: '30px', height: '30px', color: '#000', padding: '0' }}><CloseIcon /></Button>
-                    <h2>Sube una foto</h2>
-                </div>
-                <div className={styles.ContentModalUploadGrid}>
-                    <div>
-                        <CloudQueueIcon />
-                    </div>
-                    <Button component="label" variant="contained" color="success">
-                        Seleccionar desde la computadora
-                        <VisuallyHiddenInput type="file" />
-                    </Button>
-                </div>
+      fetch('http://localhost:3000/uploads', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Network response was not ok.');
+        })
+        .then((data) => {
+          console.log('File uploaded:', data);
 
-            </div>
-        </React.Fragment>
+          // Segunda petición POST a localhost:3000/posts
+          return fetch('http://localhost:3000/posts', {
+            method: 'POST',
+          });
+        })
+        .then((response) => {
+          if (response.ok) {
+            console.log('Post created successfully');
+            // Realizar acciones adicionales si es necesario con la respuesta del servidor
+          } else {
+            throw new Error('Network response was not ok.');
+          }
+        })
+        .catch((error) => {
+          console.error('There was an error:', error);
+          // Manejar el error de la solicitud
+        });
+    }
+  };
 
-    );
+  return (
+    <React.Fragment>
+      <div className={styles.modalContainer}>
+        <div className={styles.headModalUploadImage}>
+          <Button style={{ width: '30px', height: '30px', color: '#000', padding: '0' }}>
+            <CloseIcon />
+          </Button>
+          <h2>Sube una foto</h2>
+          <Button onClick={handleUpload} variant="contained" color="primary">
+            Publicar
+          </Button>
+        </div>
+        <div className={styles.ContentModalUploadGrid}>
+          <div>
+            <CloudQueueIcon />
+          </div>
+          <Button component="label" variant="contained" color="success">
+            Seleccionar desde la computadora
+            <input type="file" onChange={handleFileChange} style={{ display: 'none' }} name='image'/>
+          </Button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
